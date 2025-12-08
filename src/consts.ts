8ae846baa69a341bd25550e01c2527fce7263ab1
water_wind_ <blwaterwind@gmail.com>
2025-12-07 19:26:23 -0500

    1

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Ross Creek Cabinets | Custom Cabinets in Waycross, GA";
+export const SITE_DESCRIPTION =
+	"Custom cabinets, built-ins, and storage solutions by Derek Roberson at Ross Creek Cabinets LLC in Waycross, Georgia.";
